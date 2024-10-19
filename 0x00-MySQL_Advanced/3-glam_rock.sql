6d3aa1863af387549e4c4ae4e50877ee03999140
ArtaxJay <okjaceauthor@gmail.com>
2024-10-20 00:19:59 +0100

    Add 3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- This script will select:
+-- band_name, and lifespan columns
+
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
